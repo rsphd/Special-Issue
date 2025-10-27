1de985a1b0f103acb749a89b5175b65e0dd768d7
rsphd <164235023+rsphd@users.noreply.github.com>
2025-10-27 17:52:57 +0530

    Update p4t5_UniformPeriods_Equi_Tri_ratio_tooless_1.R

--- p4t5_UniformPeriods_Equi_Tri_ratio_tooless_1.R
+++ p4t5_UniformPeriods_Equi_Tri_ratio_tooless_1.R
@@ -1,3 +1,4 @@
+#Efficiency of Uniform on periods design with p=4, t=5 and n=20 with sigma_1^2/sigma_2^2=0.001
 library(MASS)
 library(pracma)
 
@@ -136,5 +137,6 @@ p4t5_UniformPeriods_Equi_Tri_ratio_tooless_1=read_excel("D:/Special Issue/p4t5_U
 min(p4t5_UniformPeriods_Equi_Tri_ratio_tooless_1$Efficiency) #Minimum efficiency
 
 max(p4t5_UniformPeriods_Equi_Tri_ratio_tooless_1$Efficiency) #Maximum efficiency
+
 
 
