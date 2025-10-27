c8b08b7484ae5127862de98ce3f8da4b14d2ca93
rsphd <164235023+rsphd@users.noreply.github.com>
2025-10-27 17:55:22 +0530

    Update p4t7_UniformPeriods_Equi_Tri_ratio_toogreater_1.R

--- p4t7_UniformPeriods_Equi_Tri_ratio_toogreater_1.R
+++ p4t7_UniformPeriods_Equi_Tri_ratio_toogreater_1.R
@@ -1,3 +1,4 @@
+#Efficiency of Uniform on periods design with p=4, t=7 and n=42 with sigma_1^2/sigma_2^2=100
 library(MASS)
 library(pracma)
 
@@ -158,4 +159,5 @@ p4t7_UniformPeriods_Equi_Tri_ratio_toogreater_1=read_excel("D:/Special Issue/p4t
 min(p4t7_UniformPeriods_Equi_Tri_ratio_toogreater_1$Efficiency) #Minimum efficiency
 
 max(p4t7_UniformPeriods_Equi_Tri_ratio_toogreater_1$Efficiency) #Maximum efficiency
+
 
